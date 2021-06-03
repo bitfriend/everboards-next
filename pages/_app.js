617bc07bf83ec85a905ce95343c914cb33a9b891
bitfriend <bitfriend@hotmail.com>
2021-06-04 00:28:26 +0200

    Introduce material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import createCache from '@emotion/cache';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import theme from '../src/helpers/theme';
+import { CacheProvider } from '@emotion/react';
+
+export const cache = createCache({
+  key: 'css',
+  prepend: true
+});
+
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Remove the server-side injected CSS
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired
+};
--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Box, Container, Typography } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Container,
+  Typography
+} from '@material-ui/core';
+
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 
-export default function Index() {
+export default function About() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js v5-alpha example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <Button variant="contained" component={Link} noLinkStyle href="/">
+          Go to the main page
+        </Button>
         <ProTip />
         <Copyright />
       </Box>
