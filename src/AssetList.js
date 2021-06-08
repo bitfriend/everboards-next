5bd9457329d2948d990921cf9ec709901bbd465d
bitfriend <bitfriend@hotmail.com>
2021-06-08 21:20:54 +0200

    Make grid reorderable via drag&drop

--- src/AssetList.js
+++ src/AssetList.js
@@ -1,36 +1,36 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
 import {
   Box,
   CircularProgress,
-  GridList,
-  GridListTile,
-  makeStyles
+  makeStyles,
+  withStyles
 } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 import NextImage from 'next/image';
 import MuiImage from 'material-ui-image';
 import NextLink from 'next/link';
+import {
+  SortableContainer,
+  SortableElement,
+  SortableHandle
+} from 'react-sortable-hoc';
+import { IoEye } from 'react-icons/io5';
+import arrayMove from 'array-move';
 import PropTypes from 'prop-types';
+
+import theme from '../src/helpers/theme';
 
 const useStyles = makeStyles(theme => ({
   loading: {
     height: theme.spacing(5),
     textAlign: 'center'
   },
-  tile: {
-    height: '100% !important',
-    padding: `${theme.spacing(1)}px !important`,
-    [theme.breakpoints.only('xs')]: {
-      padding: `${theme.spacing(0.5)}px !important`
-    }
-  },
-  assetThumb: {
+  thumb: {
     backgroundColor: '#fff',
     padding: theme.spacing(1),
     [theme.breakpoints.only('xs')]: {
       padding: theme.spacing(0.5)
-    },
-    cursor: 'pointer'
+    }
   }
 }));
 
@@ -39,40 +39,131 @@ function getAssetImage({ image_original_url, image_preview_url, image_thumbnail_
   return image_original_url || image_preview_url || image_thumbnail_url || image_url || '/images/placeholder.png';
 }
 
-const AssetList = forwardRef((props, ref) => {
-  const { data, error, onItemClicked } = props;
+const StyledHandle = withStyles(theme => ({
+  root: {
+    position: 'absolute',
+    top: theme.spacing(2.5),
+    right: theme.spacing(2.5),
+    width: theme.spacing(5),
+    height: theme.spacing(5),
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#000',
+    borderRadius: theme.spacing(0.75),
+    backgroundColor: '#fff',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer'
+  }
+}))(Box);
 
+const DragHandle = SortableHandle(({ value, onClicked }) => (
+  <StyledHandle className="asset-handle">
+    <NextLink
+      className="asset-handle"
+      href={`/assets?contract_address=${value.asset_contract.address}&token_id=${value.token_id}`}
+    >
+      <a
+        className="asset-handle"
+        onClick={() => onClicked(value.asset_contract.address, value.token_id)}
+      >
+        <IoEye className="asset-handle" size={theme.spacing(4)} style={{
+          marginTop: theme.spacing(0.5),
+          pointerEvents: 'none'
+        }} />
+      </a>
+    </NextLink>
+  </StyledHandle>
+))
+
+const StyledTile = withStyles(theme => ({
+  root: {
+    float: 'left',
+    display: 'inline',
+    width: '50%',
+    padding: theme.spacing(1),
+    [theme.breakpoints.only('sm')]: {
+      padding: theme.spacing(0.5)
+    },
+    position: 'relative',
+    cursor: 'grab'
+  }
+}))(Box);
+
+const SortableItem = SortableElement(({ value, onClicked }) => {
   const classes = useStyles();
 
-  return error ? (
+  return (
+    <StyledTile>
+      <MuiImage
+        src={getAssetImage(value)}
+        errorIcon={( // finish loading if no image exists in url
+          <NextImage
+            src="/images/placeholder.png"
+            alt=""
+            width="100%"
+            height="100%"
+          />
+        )}
+        className={classes.thumb}
+      />
+      <DragHandle value={value} onClicked={onClicked} />
+    </StyledTile>
+  );
+})
+
+const StyledContainer = withStyles({
+  root: {
+    display: 'flow-root'
+  }
+})(Box);
+
+const SortableGrid = SortableContainer(({ items, onItemClicked }) => (
+  <StyledContainer>
+    {items.map((item, index) => (
+      <SortableItem
+        key={`item-${item.id}`}
+        index={index} // required
+        value={item}
+        onClicked={onItemClicked}
+      />
+    ))}
+  </StyledContainer>
+))
+
+const AssetList = forwardRef((props, ref) => {
+  const [items, setItems] = useState();
+  const classes = useStyles();
+
+  useEffect(() => {
+    setItems(props.data ? props.data.assets : null);
+  }, [props.data]);
+
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    setItems(items => arrayMove(items, oldIndex, newIndex));
+  }
+
+  return props.error ? (
     <Alert severity="error">Failed to load data</Alert>
-  ) : !data ? (
+  ) : !items ? (
     <Box className={classes.loading}>
       <CircularProgress />
     </Box>
   ) : (
-    <GridList cols={2}>
-      {data.assets.map((asset, index) => (
-        <GridListTile key={index} className={classes.tile}>
-          <NextLink href={`/assets?contract_address=${asset.asset_contract.address}&token_id=${asset.token_id}`}>
-            <a onClick={() => onItemClicked(asset.asset_contract.address, asset.token_id)}>
-              <MuiImage
-                src={getAssetImage(asset)}
-                errorIcon={( // finish loading if no image exists in url
-                  <NextImage
-                    src="/images/placeholder.png"
-                    alt=""
-                    width="100%"
-                    height="100%"
-                  />
-                )}
-                className={classes.assetThumb}
-              />
-            </a>
-          </NextLink>
-        </GridListTile>
-      ))}
-    </GridList>
+    <SortableGrid
+      axis="xy"
+      items={items}
+      onSortEnd={onSortEnd}
+      onItemClicked={props.onItemClicked}
+      shouldCancelStart={event => {
+        // Cancel sorting if the event target is an `input`, `textarea`, `select` or `option`
+        // So that user can drag & drop asset
+        if (event.target.classList.contains('asset-handle')) {
+          return true;
+        }
+      }}
+    />
   );
 })
 
